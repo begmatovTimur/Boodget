6bcec7c6e67ad2418f24a75ebc067d815544dfbd
begmatovTimur <tbegmatov40@gmail.com>
2025-08-22 10:16:44 +0500

    trying to fix deploy bug 6th try

---
+++ frontend/boodget-frontend/next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: 'export',
+    // Optional, uncomment if you want clean `/page/` URLs instead of `/page.html`
+    // trailingSlash: true,
+}
+
+module.exports = nextConfig
