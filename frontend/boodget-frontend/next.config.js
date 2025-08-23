b2adca711bd37edd9ae7046e78ad240e903a437a
begmatovTimur <tbegmatov40@gmail.com>
2025-08-23 16:58:33 +0500

    static routes bug fixed

--- frontend/boodget-frontend/next.config.js
+++ frontend/boodget-frontend/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    // Optional, uncomment if you want clean `/page/` URLs instead of `/page.html`
-    // trailingSlash: true,
+    // no output: 'export'
 }
 
 module.exports = nextConfig
